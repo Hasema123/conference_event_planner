6e2a5216aebcdce16badec519ae77851359ba9e3
Hasema123 <dbauiwe@outlook.de>
2024-07-10 15:00:22 +0200

    image paths

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
     {
-      img: "../assets/projector-20031_1920.jpg",
+      img: "assets/projector-20031_1920.jpg",
       name: "Projectors",
       cost: 200,
       quantity: 0,
     },
     {
-      img: "../assets/speakers-4109274_1920.jpg",
+      img: "assets/speakers-4109274_1920.jpg",
       name: "Speaker",
       cost: 35,
       quantity: 0,
     },
     {
-      img: "../assets/microphone-3926344_1920.jpg",
+      img: "assets/microphone-3926344_1920.jpg",
       name: "Microphones",
       cost: 45,
       quantity: 0,
     },
     {
-      img: "../assets/whiteboard-2903269_1920.png",
+      img: "assets/whiteboard-2903269_1920.png",
       name: "Whiteboards",
       cost: 80,
       quantity: 0,
     },
 
     {
-      img: "../assets/signpost-235079_1920.jpg",
+      img: "assets/signpost-235079_1920.jpg",
       name: "Signage",
       cost: 80,
       quantity: 0,
