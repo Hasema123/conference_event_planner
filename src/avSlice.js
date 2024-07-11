7b71f4dad9d545021a7f8a3cae2c3bbfbfd21053
Hasema123 <dbauiwe@outlook.de>
2024-07-11 15:00:12 +0200

    ...

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,33 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
     {
-      img: "/conference_event_planner/public/projector-20031_1920.jpg",
+      // img: "/conference_event_planner/public/projector-20031_1920.jpg",
+      img: "/projector-20031_1920.jpg",
       name: "Projectors",
       cost: 200,
       quantity: 0,
     },
     {
-      img: "/conference_event_planner/public/speakers-4109274_1920.jpg",
+      img: "/speakers-4109274_1920.jpg",
       name: "Speaker",
       cost: 35,
       quantity: 0,
     },
     {
-      img: "/conference_event_planner/public/microphone-3926344_1920.jpg",
+      img: "/microphone-3926344_1920.jpg",
       name: "Microphones",
       cost: 45,
       quantity: 0,
     },
     {
-      img: "/conference_event_planner/public/whiteboard-2903269_1920.png",
+      img: "/whiteboard-2903269_1920.png",
       name: "Whiteboards",
       cost: 80,
       quantity: 0,
     },
 
     {
-      img: "/conference_event_planner/public/signpost-235079_1920.jpg",
+      img: "/signpost-235079_1920.jpg",
       name: "Signage",
       cost: 80,
       quantity: 0,
